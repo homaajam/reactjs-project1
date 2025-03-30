cba62b33984f4310ae96d35a8d0dbf58179a6d8a
Homa <hmajam83@gmail.com>
2025-03-30 12:48:12 +0330

    add prop to Book component

--- src/components/book.js
+++ src/components/book.js
@@ -1,6 +1,4 @@
-const Book=()=>{
-  
-  
+const Book=({book})=>{
   
   return(
   <li>
@@ -12,7 +10,7 @@ const Book=()=>{
             width: 128,
             height: 193,
             backgroundImage:
-              'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+              'url(${book.image})',
           }}
         ></div>
         <div className="book-shelf-changer">
@@ -29,8 +27,8 @@ const Book=()=>{
           </select>
         </div>
       </div>
-      <div className="book-title">To Kill a Mockingbird</div>
-      <div className="book-authors">Harper Lee</div>
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.author}</div>
     </div>
   </li>)
                 
