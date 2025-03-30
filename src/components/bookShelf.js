a95879d13c1f8e36bd2351c3ba26c5b208282273
Homa <hmajam83@gmail.com>
2025-03-30 13:28:56 +0330

    create bookshelf component

---
+++ src/components/bookShelf.js
@@ -0,0 +1,18 @@
+import Book from './book';
+const BookShelf=({title ,books})=>{
+
+  return(
+    <div className="bookshelf">
+        <h2 className="bookshelf-title">{title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map((book)=>{
+              <Book key={book.id} book={book} />
+            })}
+          
+          </ol>
+        </div>
+    </div>
+  )
+ 
+}
