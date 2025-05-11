d56cd79a91b09dacbdf0393efee627cdec36891d
Homa <hmajam83@gmail.com>
2025-05-11 13:35:28 +0330

    add updateShelf prop

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,37 +1,8 @@
 import BookShelf from "./bookShelf";
 import {link} from "react-router-dom";
 
-const book =[
-  {
-    id: 1,
-    title: "To Kill a Mockingbird",
-    author: "Harper Lee",
-    image: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-    shelf: "Currently Reading",
-  },
-  {
-    id: 2,
-    title: "Ender's Game",
-    author: "Orson Scott Card",
-    image: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-    shelf: "currentlyReading",
-  },
-  {
-    id: 3,
-    title: "1776",
-    author: "David McCullough",
-    image: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-    shelf: "wantToRead",
-  },
-  {
-    id: 4,
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    image: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
-    shelf: "read",
-  },
-];
-const BookList=(books)=>{
+
+const BookList=({books,updateShelf})=>{
 
   return(
     <div className="list-books">
@@ -40,13 +11,13 @@ const BookList=(books)=>{
       </div>
       <div className="list-books-content">
         <BookShelf title={"Currently Reading"} books={
-          books.filter((book)=> book.shelf === "Currently Reading")}
+          books.filter((book)=> book.shelf === "Currently Reading")} updateShelf={updateShelf}
         />
         <BookShelf title={"Want to Read"} books={
-          books.filter((book)=> book.shelf === "Want to Read")}
+          books.filter((book)=> book.shelf === "Want to Read")} updateShelf={updateShelf}
         />
         <BookShelf title={"read"} books={
-          books.filter((book)=> book.shelf === "read")}
+          books.filter((book)=> book.shelf === "read")} updateShelf={updateShelf}
         />
       </div>
       <div className="open-search">
@@ -55,3 +26,4 @@ const BookList=(books)=>{
     </div>
   )
 }
+export default BookList;
