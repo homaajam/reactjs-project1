e0d559adbc63ceff648e6bdb54df5bf648a4a934
Homa <hmajam83@gmail.com>
2025-05-16 20:36:32 +0330

    fix API object names

--- src/components/book.js
+++ src/components/book.js
@@ -14,7 +14,7 @@ const Book=({book,updateShelf})=>{
             width: 128,
             height: 193,
             backgroundImage:
-              `url(${book.image})`,
+              `url(${book.imageLinks})`,
           }}
         ></div>
         <div className="book-shelf-changer">
@@ -32,7 +32,7 @@ const Book=({book,updateShelf})=>{
         </div>
       </div>
       <div className="book-title">{book.title}</div>
-      <div className="book-authors">{book.author}</div>
+      <div className="book-authors">{book.authors}</div>
     </div>
   </li>)
                 
